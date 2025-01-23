59c978fa440d73925bb4b252b18041429681327e
Sanman365 <ariel2906to@gmail.com>
2025-01-23 12:46:30 -0600

    Update results.js

--- results.js
+++ results.js
@@ -4,15 +4,17 @@ const searchResults = JSON.parse(localStorage.getItem('searchResults'));
 if (searchResults && searchResults.length > 0) {
     searchResults.forEach(video => {
         console.log("Cargando video:", video.title, "Link:", window.location.origin + "/" + video.link); // 🔍 Depuración
-        
+
         const videoCard = document.createElement('div');
-        videoCard.classList.add('image-gallery');
+        videoCard.classList.add('video-card');
+
         videoCard.innerHTML = `
             <a href="${video.link}">
                 <img src="${video.image}" alt="${video.title}">
             </a>
-            <div class="baby-driver-container><h3 class="baby-driver-text>${video.title}</h3 ></div> 
+            <h3 class="baby-driver-text">${video.title}</h3>
         `;
+
         resultsContainer.appendChild(videoCard);
     });
 } else {
